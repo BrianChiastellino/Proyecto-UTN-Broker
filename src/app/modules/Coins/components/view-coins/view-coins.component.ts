f8c35b8ab175bed546af52713e7b5c089029e11b
TomAlberto99 <tomasalberto.alberto@gmail.com>
2023-11-06 03:51:30 -0300

    boton ver mas bsucador

--- src/app/modules/Coins/components/view-coins/view-coins.component.ts
+++ src/app/modules/Coins/components/view-coins/view-coins.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Router } from '@angular/router';
 import { Coin } from 'src/app/core/Models';
 
 @Component({
@@ -12,19 +13,33 @@ export class ViewCoinsComponent implements OnChanges {
   @Input() coinsView: Array<Coin> = [];
   coinsFiltred: Array<Coin> = [];
 
+
   coinToSearch: string = '';
   mostrarMas: boolean = false;
 
   userIsLoged: boolean = true;
 
+
+  
+
+
+
+  constructor(private router:Router){
+
+
+  }
   //!Se utiliza ngOnChanges para reaccionar a los cambios del @Input que recibe el componente.
   //!Si no se hace este metodo  'coins2' seria null y no funcionaria el filtrado de busqueda.
   ngOnChanges(changes: SimpleChanges): void {
     this.coinsFiltred = [...this.coinsView]
+
+
   }
 
   public verMas(): void {
-    this.mostrarMas = !this.mostrarMas;
+    this.router.navigate(['/main']);
+    this.mostrarMas = true;
+
   }
 
   public buscarCoin(): void {
--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoinsModule } from '../Coins/coins.module';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { SharedModule } from "../../shared/shared.module";
+
 
 
 
 @NgModule({
-  declarations: [
-    MainPageComponent
-  ],
-  imports: [
-    CommonModule,
-    CoinsModule
-  ]
+    declarations: [
+        MainPageComponent,
+    ],
+    imports: [
+        CommonModule,
+        CoinsModule,
+        SharedModule
+    ]
 })
 export class MainModule { }
