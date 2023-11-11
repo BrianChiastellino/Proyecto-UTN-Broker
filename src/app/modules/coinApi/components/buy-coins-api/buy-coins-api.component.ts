8aa501e383c7e8d572a73ea98128a6ffd3758f52
TomAlberto99 <tomasalberto.alberto@gmail.com>
2023-11-11 05:47:09 -0300

    formulario compra criptos

--- src/app/modules/coinApi/components/buy-coins-api/buy-coins-api.component.ts
+++ src/app/modules/coinApi/components/buy-coins-api/buy-coins-api.component.ts
@@ -11,24 +11,44 @@ export class BuyCoinsApiComponent implements OnChanges{
 
   coinCompra!: CoinApi;
   @Input() coinSelected!: CoinApi;
-  showForm = false;
+  showForm = true;
+
+  cantidad : number = 0;
+  pesos : number = 0;
+  valorCompra : number = 0;
+  valorCompraPesos : number = 0;
 
   ngOnChanges(changes: SimpleChanges): void {
     console.log('Desde buy',this.coinSelected);
     this.coinCompra = this.coinSelected;
     console.log('Desde el this', this.coinCompra);
-    this.toggleForm()
+
+
   }
 
 
 
   toggleForm() {
-    if(this.coinSelected){
 
-      this.showForm = true;
-    }
+
+      this.showForm = !this.showForm;
+
   }
 
+  calcularPrecio() {
+
+  
+    this.valorCompra = this.cantidad * this.coinSelected.current_price;
+  }
+
+  calcularCompra() {
+
+      this.cantidad = this.pesos / this.coinSelected.current_price;
+
+      this.valorCompraPesos = this.cantidad * this.coinSelected.current_price;
+
+  }
+  
 
 
 
